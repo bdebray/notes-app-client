1624a715b82d03814c411b9f16de220abf95b88b
Blake DeBray <bdebray@aais.com>
2019-01-28 12:22:59 -0600

    Load and clear session state and add redirect on login/logout.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { LinkContainer } from "react-router-bootstrap";
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 import "./App.css";
 import Routes from "./Routes";
 
@@ -11,16 +12,35 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
+
     this.userHasAuthenticated(false);
+    
+    this.props.history.push("/login");
   }
 
   render() {
@@ -30,6 +50,7 @@ class App extends Component {
     };
 
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -60,4 +81,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
