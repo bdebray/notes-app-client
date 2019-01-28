5d8506efe6e97a9d6f3b288659c26709b47cdc87
Blake DeBray <bdebray@aais.com>
2019-01-28 11:15:31 -0600

    Add initial app from tutorial

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "deb-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://m84a875xq9.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_jiNj2zVZ0",
+        APP_CLIENT_ID: "4omh98f81ptbhrv3j03mbb9926",
+        IDENTITY_POOL_ID: "us-east-2:54738c46-7bc6-413f-bc9b-fc52244f9b55"
+    }
+};
